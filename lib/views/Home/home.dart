0adbc3cf4a4cc6ff2520da01f83bbbb264425743
MiceliLanda <183381@ids.upchiapas.edu.mx>
2022-06-12 16:04:59 -0500

    searchBar&&ProductsBar

--- lib/views/Home/home.dart
+++ lib/views/Home/home.dart
@@ -4,6 +4,7 @@ import 'dart:async';
 import 'dart:convert';
 import 'package:consuming_api/views/Login/login_view.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:http/http.dart' as http;
 
 import '../../models/caroussel.dart';
@@ -108,6 +109,7 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       appBar: AppBar(
         centerTitle: true,
         title: Row(
@@ -145,51 +147,189 @@ class _HomeState extends State<Home> {
           // color: Colors.grey,
           // height: double.infinity,
           margin: const EdgeInsets.only(right: 5, left: 5, bottom: 10, top: 0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              _headItems(),
-              Container(
-                  margin: const EdgeInsets.all(0), child: _scrollHoriontal()),
-
-              _sliderImage(),
-              // Expanded(child: _pageChanged()),
-              _gridImages(context)
-            ],
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                _headItems(),
+                Container(
+                  margin: const EdgeInsets.all(0),
+                  child: _scrollHoriontal(),
+                ),
+                _searchBar(),
+                _sliderImage(),
+                const Divider(
+                  color: Colors.grey,
+                  thickness: 1,
+                ),
+                Container(
+                  height: 50,
+                  // color: Colors.grey,
+                  child: Row(
+                    children: [
+                      const Text(
+                        'Productos cerca  ',
+                        style: TextStyle(fontSize: 20),
+                      ),
+                      Row(
+                        children: [
+                          // list view to text with scroll horizontal
+                          Container(
+                            width: 225,
+                            height: 20,
+                            alignment: Alignment.center,
+                            // color: Colors.red,
+                            child: ListView.builder(
+                              scrollDirection: Axis.horizontal,
+                              itemCount: images.length,
+                              itemBuilder: (context, index) {
+                                // return Padding(
+                                return Padding(
+                                  padding: const EdgeInsets.only(right: 8.0),
+                                  child: ElevatedButton(
+                                    onPressed: null,
+                                    child: Text(
+                                      images[index]['name'].toString(),
+                                      // "aaa",
+                                      style: const TextStyle(
+                                        fontSize: 15,
+                                        color: Colors.white,
+                                      ),
+                                    ),
+                                    style: ButtonStyle(
+                                      backgroundColor:
+                                          MaterialStateProperty.all(
+                                        Colors.green,
+                                      ),
+                                      shape: MaterialStateProperty.all<
+                                          RoundedRectangleBorder>(
+                                        RoundedRectangleBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(18.0),
+                                          side: const BorderSide(
+                                              color: Colors.grey),
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                );
+                                //   padding: const EdgeInsets.only(right: 8.0),
+                                //   child: Text(
+                                //     "${images[index]['name']}",
+                                //     style: const TextStyle(
+                                //         fontSize: 18, color: Colors.purple),
+                                //     textAlign: TextAlign.center,
+                                //   ),
+                                // );
+                              },
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+                // Expanded(child: _pageChanged()),
+                _gridImages(context)
+              ],
+            ),
           ),
         ),
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        backgroundColor: const Color(0xff4f1581),
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.person,
-              color: Colors.white,
-            ),
-            label: 'Profile',
+      // bottomNavigationBar: _bottomNavigation(context),
+    );
+  }
+
+  BottomNavigationBar _bottomNavigation(BuildContext context) {
+    return BottomNavigationBar(
+      backgroundColor: const Color(0xff4f1581),
+      items: const [
+        BottomNavigationBarItem(
+          icon: Icon(
+            Icons.person,
+            color: Colors.white,
           ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.home,
-              color: Colors.white,
-            ),
-            label: 'Home',
+          label: 'Profile',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(
+            Icons.home,
+            color: Colors.white,
           ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.logout,
-              color: Colors.white,
-            ),
-            label: 'Logout',
+          label: 'Home',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(
+            Icons.logout,
+            color: Colors.white,
           ),
+          label: 'Logout',
+        ),
+      ],
+      onTap: (index) {
+        if (index == 2) {
+          Navigator.pushReplacement(
+              context, MaterialPageRoute(builder: (context) => const Login()));
+        }
+      },
+    );
+  }
+
+  Container _searchBar() {
+    return Container(
+      margin: const EdgeInsets.only(left: 10),
+      // color: Colors.grey,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Container(
+            height: 35,
+            width: 325,
+            margin: const EdgeInsets.only(top: 25),
+            child: TextField(
+              textAlign: TextAlign.center,
+              textAlignVertical: TextAlignVertical.bottom,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                hintText: 'Buscar productos o servicios...',
+                suffixIcon: const Icon(Icons.search),
+              ),
+            ),
+          ),
+          // button circle icon filter
+          Container(
+            width: 45,
+            height: 35,
+            margin: const EdgeInsets.only(top: 20),
+            child: Center(
+              child: MaterialButton(
+                onPressed: () {},
+                color: Colors.pink,
+                // textColor: Colors.white,
+                child: const Icon(
+                  Icons.format_list_bulleted_outlined,
+                  size: 20,
+                  textDirection: TextDirection.ltr,
+                  color: Colors.white,
+                ),
+                // padding: EdgeInsets.all(1),
+                shape: const CircleBorder(),
+              ),
+            ),
+          )
+
+          // Container(
+          //   height: 35,
+          //   width: 20,
+          //   margin: const EdgeInsets.only(right: 10),
+          //   child: const Icon(
+          //     Icons.format_list_bulleted_outlined,
+          //     color: Colors.purple,
+          //   ),
+          // ),
         ],
-        onTap: (index) {
-          if (index == 2) {
-            Navigator.pushReplacement(context,
-                MaterialPageRoute(builder: (context) => const Login()));
-          }
-        },
       ),
     );
   }
@@ -274,7 +414,8 @@ class _HomeState extends State<Home> {
         value: dropdownValue2,
         icon: const Icon(Icons.arrow_drop_down),
         elevation: 0,
-        style: const TextStyle(color: Colors.black54),
+        style:
+            const TextStyle(color: Colors.black54, fontWeight: FontWeight.bold),
         underline: Container(
           height: 0,
           color: Colors.black54,
@@ -308,7 +449,8 @@ class _HomeState extends State<Home> {
         value: dropdownValue,
         icon: const Icon(Icons.arrow_drop_down),
         elevation: 0,
-        style: const TextStyle(color: Colors.black54),
+        style:
+            const TextStyle(color: Colors.black54, fontWeight: FontWeight.bold),
         underline: Container(
           height: 0,
           color: Colors.black54,
@@ -331,60 +473,55 @@ class _HomeState extends State<Home> {
     );
   }
 
-  Expanded _scrollHoriontal() {
-    return Expanded(
-      flex: 1,
-      child: Column(
-        // mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          SizedBox(
-            // color: Colors.greenAccent,}
-            height: 180,
-            child: ListView.builder(
-              itemCount: images.length,
-              scrollDirection: Axis.horizontal,
-              itemBuilder: (context, index) => Column(
-                children: [
-                  Container(
-                    margin: const EdgeInsets.only(right: 20),
-                    height: 180,
-                    decoration: BoxDecoration(
-                      // color: Colors.redAccent,
-                      borderRadius: BorderRadius.circular(10),
-                      border: Border.all(
-                        color: const Color(0xff4f1581),
-                        width: 1,
-                      ),
-                    ),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Image.asset(images[index]['image'].toString(),
-                            width: 140, height: 120),
-                        Text(images[index]['name'].toString(),
-                            style: const TextStyle(
-                                fontSize: 20,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.green)),
-                      ],
+  Column _scrollHoriontal() {
+    return Column(
+      // mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        SizedBox(
+          // color: Colors.greenAccent,}
+          height: 180,
+          child: ListView.builder(
+            itemCount: images.length,
+            scrollDirection: Axis.horizontal,
+            itemBuilder: (context, index) => Column(
+              children: [
+                Container(
+                  margin: const EdgeInsets.only(right: 20),
+                  height: 180,
+                  decoration: BoxDecoration(
+                    // color: Colors.redAccent,
+                    borderRadius: BorderRadius.circular(10),
+                    border: Border.all(
+                      color: const Color(0xff4f1581),
+                      width: 1,
                     ),
                   ),
-                ],
-              ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset(images[index]['image'].toString(),
+                          width: 140, height: 120),
+                      Text(images[index]['name'].toString(),
+                          style: const TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.green)),
+                    ],
+                  ),
+                ),
+              ],
             ),
-          )
-        ],
-      ),
+          ),
+        )
+      ],
     );
   }
 
-  Expanded _gridImages(BuildContext context) {
-    return Expanded(
-      flex: 2,
-      child: Container(
-          // height: 60,
-          // margin: const EdgeInsets.only(top: 0),
-          child: GridView.count(
+  Container _gridImages(BuildContext context) {
+    return Container(
+      // flex: 2,
+      height: 150,
+      child: GridView.count(
         physics: const BouncingScrollPhysics(),
         crossAxisCount: 2,
         children: List.generate(images.length, (index) {
@@ -441,25 +578,27 @@ class _HomeState extends State<Home> {
               color: Colors.white,
               shape: BeveledRectangleBorder(
                 borderRadius: BorderRadius.circular(10),
-                side: BorderSide(color: Color.fromARGB(255, 136, 136, 136)),
+                side:
+                    const BorderSide(color: Color.fromARGB(255, 136, 136, 136)),
               ),
-              child: Image.asset(images[index]['image'].toString(),
-                  width: 10, height: 11),
+              child: Image.asset(
+                images[index]['image'].toString(),
+              ),
             ),
           );
         }),
-      )),
+      ),
     );
   }
 
-  Container _sliderImage() {
-    return Container(
+  SizedBox _sliderImage() {
+    return SizedBox(
       // color: Colors.yellowAccent,
 
       height: 170,
       // color: Colors.red,
       // margin: const EdgeInsets.only(top: 0, left: 5, right: 5, bottom: 0),
-      width: 355,
+      width: 365,
       child: Column(
         children: [
           Expanded(
